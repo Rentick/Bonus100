c497f603c3477e14e70b6dbb9afcc9d0c20e5079
Renat Khaybullin <zabzari@gmail.com>
2020-09-11 19:58:24 +0300

    Bonus1 commit

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@ public class Main {
         int amountClient = 200;
         long amount = 1200;
         long bonusAmount = 100;
-        long totalBonus = amount / bonusAmount;
+        long totalBonus = 0;
         long total = amountClient + amount + totalBonus;
         long limit = 1000;
         if (amount > limit) {
-            total = amountClient + amount + totalBonus;;
+            totalBonus = amount / bonusAmount;
         }
         else {
-            total = amountClient + amount;
+            totalBonus = 0;
         }
 
-        System.out.println(total);
+        System.out.println(totalBonus);
     }
 }
