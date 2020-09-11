dd4a80df6b4c9a2f62ff8ed758de4108f2a2c097
Renat Khaybullin <zabzari@gmail.com>
2020-09-11 20:03:56 +0300

    Bonus2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int amountClient = 200;
-        long amount = 1200;
+        long amount = 1100;
         long bonusAmount = 100;
         long totalBonus = 0;
-        long total = amountClient + amount + totalBonus;
         long limit = 1000;
         if (amount > limit) {
             totalBonus = amount / bonusAmount;
